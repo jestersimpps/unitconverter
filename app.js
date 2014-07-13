27208841f0dca43b17572564100b1e2df93306e9
jestersimpps <jozzzzen@jovinkeroyesmbp.home>
2014-07-13 16:59:23 -0400

    tabswitching

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 (function(){
+    var app = angular.module('uc',[]);   
+    
+    
+    app.controller('ElementsController',function(){
+	
+    this.lengthelements = lengths;
+    
+    });  
+    
+    var lengths =
+    [
+      {name: 'meter', tometer: 1},
+      {name: 'yard', tometer: 0.9144},
+      {name: 'centimeter', tometer: 0.01},	
+      {name: 'inch', tometer: 0.0254},
+      {name: 'kilometer', tometer: 100000},
+      {name: 'mile', tometer: 1609.344},
+      {name: 'millimeter', tometer: 0.001}
+    ];
 
-
-var app = angular.module('uc',[]);   
-
-
-app.controller('elements',function(){
-    
-    
-    
-});  
-    
-    
     
 })();
