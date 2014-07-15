e8510de3b9b2ec1b695016f367cef3bdade141fc
jestersimpps <jestersimpps@gmail.com>
2014-07-15 00:30:22 -0400

    working length calculations
    figure out remove button

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
     
     var lengths =
     [
-      {name: 'millimeters', tometer: 0.001},
-      {name: 'centimeters', tometer: 0.01},	
-      {name: 'inches', tometer: 0.0254},
-      {name: 'feet' , tometer: 0.3048},
-      {name: 'meters', tometer: 1},
-      {name: 'yards', tometer: 0.9144},
-      {name: 'kilometers', tometer: 100000},
-      {name: 'miles', tometer: 1609.344}
+      {name: 'millimeters', conv: 0.001},
+      {name: 'centimeters', conv: 0.01},	
+      {name: 'inches', conv: 0.0254},
+      {name: 'feet' , conv: 0.3048},
+      {name: 'meters', conv: 1},
+      {name: 'yards', conv: 0.9144},
+      {name: 'kilometers', conv: 1000},
+      {name: 'miles', conv: 1609.344}
 
     ];
     
     var rows =
     [
-	[{value: 3.14156, from: 4, result: 123456, to: 1 }],
+	[{value: '', from: 0, result: '', to: 0 }],
 	[{value: 3.14156, from: 0, result: 123456, to: 0 }],
 	[{value: 3.14156, from: 0, result: 123456, to: 0 }],
 	[{value: 3.14156, from: 0, result: 123456, to: 0 }],
@@ -41,10 +41,13 @@
 	this.selectTab = function(setTab) { this.tab = setTab; };
 	this.isSelected = function(checkTab) { return this.tab === checkTab; };
 	this.getRows = function(Tab){ return this.panelrows[this.tab];};
+	
 	this.removeRow = function(index)
 	{
-	    this.panelrows[this.tab].splice(index,1);
+
+	this.panelrows[this.tab].splice(index,1);
 	
+	    
 	};
 	
 	
@@ -56,7 +59,39 @@
 	this.changeValue= function(row,val) {
 	    
 	    this.panelrows[this.tab][row].value = val;
+	    this.calcResult(row);
+	    
+	      
 	};
+	
+	this.changeFrom= function(row,from) {
+	    
+	    var ind = this.lengthelements.indexOf(from);
+	    this.panelrows[this.tab][row].from = ind;
+	    this.calcResult(row);
+	    
+	};
+	
+	this.changeTo= function(row,to) {
+	    
+	    var ind = this.lengthelements.indexOf(to);
+	    this.panelrows[this.tab][row].to = ind;
+	    this.calcResult(row);
+	    
+	};
+	
+	this.calcResult= function(row) {
+	    
+	    value = this.panelrows[this.tab][row].value;
+	    from_index = this.panelrows[this.tab][row].from;
+	    to_index = this.panelrows[this.tab][row].to;
+	    conv_from = this.lengthelements[from_index].conv;
+	    conv_to = this.lengthelements[to_index].conv;
+	    
+	    this.panelrows[this.tab][row].result = (value/conv_to)*conv_from;
+	};
+	
+	
 	
     });
     
