227cdd6c7802fcd7030ee323b140939a6b15ab84
jestersimpps <jestersimpps@gmail.com>
2014-07-17 12:38:12 -0400

    added labeling and overview

--- app.js
+++ app.js
@@ -3,25 +3,20 @@
       
     var rows =
     [
-	[{value: '', from: 0, result: '', to: 0 }],
-	[{value: '', from: 0, result: '', to: 0 }],	
-	[{value: '', from: 0, result: '', to: 0 }],
-	[{value: '', from: 0, result: '', to: 0 }],
-	[{value: '', from: 0, result: '', to: 0 }],
-	[{value: '', from: 0, result: '', to: 0 }],
-	[{value: '', from: 0, result: '', to: 0 }]
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }],
+	[{label:'', value: '', from: 0, result: '', to: 0 }]
     ];
-
-    
 
     app.controller("PanelController",function(){
 	
 	this.tab = 0;
 	this.panelrows = rows;
-	this.panelunits = units;
-	
-	
-	
+	this.panelunits = units;	
 	this.counter = 0;
 	
 	this.selectTab = function(setTab) { this.tab = setTab; };
@@ -30,44 +25,36 @@
 	
 	this.removeRow = function(index)
 	{
-
+	    
 	this.panelrows[this.tab].splice(index,1);
 	
-	    
-	};
-	
-	
-	
+	};	
 	this.addrow = function()
 	{
 	    this.panelrows[this.tab].push({value: '', from: 0, result: '', to: 0 });  
-	};
-	
+	};	
 	this.changeValue= function(row,val) {
 	    
 	    this.panelrows[this.tab][row].value = val;
-	    this.calcResult(row);
+	    this.calcResult(row);	      
+	};	
+	this.changeLabel= function(row,label) {
 	    
-	      
-	};
-	
+	    this.panelrows[this.tab][row].label = label;	      
+	};	
 	this.changeFrom= function(row,from) {
 	    
 	    var ind = this.panelunits[this.tab].indexOf(from);
 	    this.panelrows[this.tab][row].from = ind;
 	    this.calcResult(row);
 	    
-	};
-	
+	};	
 	this.changeTo= function(row,to) {
 	    
 	    var ind = this.panelunits[this.tab].indexOf(to);
 	    this.panelrows[this.tab][row].to = ind;
-	    this.calcResult(row);
-	    
-	    
-	};
-	
+	    this.calcResult(row);    
+	}	
 	this.calcResult= function(row) {
 	    
 	    value = this.panelrows[this.tab][row].value;
@@ -76,24 +63,38 @@
 	    conv_from = this.panelunits[this.tab][from_index].conv;
 	    conv_to = this.panelunits[this.tab][to_index].conv;
 	    
-	    //if not temperature
-	    this.panelrows[this.tab][row].result = (value/conv_to)*conv_from;
-	    
-	    //if temperature
-	    
-	};
-	
+	    this.panelrows[this.tab][row].result = this.round(((value/conv_to)*conv_from),6);
+	};	
 	this.get_from= function(index) {
 	    
 	    return this.panelunits[this.tab][index];
+	
 	}
 	this.get_to= function(index) {
 	    
 	    return this.panelunits[this.tab][index];
-	}
 	
-
-
+	}
+	this.empty=function(value){
+	    
+	    if (value =='') {return true;}
+	    else {return false;}
+	    
+	}
+	this.exportData = function () {
+	    
+	    var blob = new Blob([document.getElementById('exportable').innerHTML], {
+		type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+	    });
+	    saveAs(blob, "Overview.xls");
+	    
+	};
+	this.round = function (number, decimals) {
+	    
+	    var newnumber = new Number(number+'').toFixed(parseInt(decimals));
+	    return parseFloat(newnumber);
+	    
+	}
     });
     
 
